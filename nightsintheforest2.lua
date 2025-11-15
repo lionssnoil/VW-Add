31b374e5bef1facdbb33b2e3f8999d0fde359339
dfgdgddfdffffdfgdfgd <bubbacamps912@gmail.com>
2025-11-15 06:57:52 -0800

    Update notification text for game lobby

--- nightsintheforest2.lua
+++ nightsintheforest2.lua
@@ -90,7 +90,7 @@ if game.PlaceId == 79546208627805 then
     pcall(function()
         game:GetService("StarterGui"):SetCore("SendNotification", {
             Title = "Voidware | 99 Nights In The Forest",
-            Text = "Go In Game for Voidware to load :D [You are in lobby currently]",
+            Text = "Get in a fucking game buddy",
             Duration = 10
         })
     end)
